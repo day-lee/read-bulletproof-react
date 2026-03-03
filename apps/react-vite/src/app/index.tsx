39d2f8274348cfb926b5e64fba31966c55fe4a46
Dayeon Lee <iamdayeonlee@gmail.com>
2026-03-03 17:39:38 +0000

    Add usage of react-query and react-router in CSR, SSR, RSC

--- apps/react-vite/src/app/index.tsx
+++ apps/react-vite/src/app/index.tsx
@@ -1,3 +1,14 @@
+/*
+.* Vite 환경에서는 CRS 구조
+.* Vite + React Query + React Router 
+.* 브라우저가 모든걸 다 하고있음 
+.* 
+.* 리액트 라우터의 경우, SSR 환경에서는 react-router 대신 프레임워크 내장 라우터로 대체해서 사용: App Router, Page Router)
+.* 리액트 쿼리의 경우, SSR환경에서도 많이 사용됨: 첫 렌더링 이후에는 SPA처럼 동장하므로 데이터 다시 가져올 경우 있음 
+.* 비동기 데이터 캐싱과 서버 상태관리는 SSR에서도 필요함. 
+.* RSC(React Server Component) next.13에서는 컴포넌트가 서버에서 실행됨. 서버에서 바로 데이터 페칭함           
+*/
+
 import { AppProvider } from './provider';
 import { AppRouter } from './router';
 
