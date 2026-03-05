9ed359ae5357503649a8fbd7876109adc22153fe
Dayeon Lee <iamdayeonlee@gmail.com>
2026-03-05 15:54:41 +0000

    Comment: queryOptions의 역할

--- apps/react-vite/src/features/discussions/api/get-discussions.ts
+++ apps/react-vite/src/features/discussions/api/get-discussions.ts
@@ -8,20 +8,29 @@ export const getDiscussions = (
   page = 1,
 ): Promise<{
   data: Discussion[];
+  // page 데이터 형태는 ts 타입 정의 파일에.
   meta: Meta;
 }> => {
+  // response로 받은 promise 객체 반환.
   return api.get(`/discussions`, {
     params: {
       page,
     },
   });
 };
-
+/**
+queryOptions 객체를 반환하는 함수
+loader, create-, delete- 에서 재사용됨 
+*/
 export const getDiscussionsQueryOptions = ({
   page,
 }: { page?: number } = {}) => {
+  // queryOptions함수를 사용해 옵션 미리 정의 및 재사용 : queryKey, queryFn을 받음
   return queryOptions({
+    // page 존재 여부에따라 키 배열의 모양이 달라짐: 다중 아이템 쿼리 키
+    // queryKey는 쿼리 캐시에서 데이터 식별하는 중요한 단서.
     queryKey: page ? ['discussions', { page }] : ['discussions'],
+    // default 값 page = 1 설정. 데이터를 가져오는 비동기 함수 : 데이터 or 오류 반환
     queryFn: () => getDiscussions(page),
   });
 };
